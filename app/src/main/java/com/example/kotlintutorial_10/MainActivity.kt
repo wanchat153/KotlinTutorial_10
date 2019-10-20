9fc8e1cb5f52babefaa7e9a73f7dd488b6d96d12
Wanchat <wanchatgame@gmail.com>
2019-10-20 14:06:02 +0700

    1. ประกาศเรียกใช้ val database(SQLlite)
    2.
    สร้าง table data และ test

--- app/src/main/java/com/example/kotlintutorial_10/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_10/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.kotlintutorial_10
 
+import android.content.ContentValues
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
+
+private const val  TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +20,29 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val database = baseContext.openOrCreateDatabase("sqlite-test-1.db", Context.MODE_PRIVATE, null)
+        var sql = "CREATE TABLE contacts(_id INTEGER PRIMARY KEY NOT NULL, name TEXT, phone INTEGER, email TEXT)"
+        Log.d(TAG, "onCreate: sql is $sql")
+        database.execSQL(sql)
+
+        sql = "INSERT INTO contacts(name, phone, email) VALUES('tim', 6456789, 'tim@email.com')"
+        Log.d(TAG, "onCreate: sql is $sql")
+        database.execSQL(sql)
+
+        val value = ContentValues().apply {
+            put("name", "Fred")
+            put("phone", 12345)
+            put("email", "fred@nurk.com")
+        }
+
+//        val value = ContentValues().apply {
+//            put("name", "Fred")
+//            put("phone", 12345)
+//            put("email", "fred@nurk.com")
+//        }
+
+        val generatedId = database.insert("contacts", null, value)
+        Log.d(TAG, "onCreate: record added with id $generatedId")
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
